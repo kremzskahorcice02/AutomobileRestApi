a571c621a7d2b046b6190a0e339356ec5ddeb04c
Šárka Kolářová <sar.kolaruu@gmail.com>
2023-07-13 17:22:03 +0200

    created plain project

---
+++ AutomobileRestApiApp/src/main/java/com/example/automobilerestapiapp/AutomobileRestApiAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.automobilerestapiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomobileRestApiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomobileRestApiAppApplication.class, args);
+	}
+
+}
